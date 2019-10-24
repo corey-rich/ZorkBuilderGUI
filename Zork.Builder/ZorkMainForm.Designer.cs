20a77b8fda33a0431105d2c921588ec467dfd2a7
corey-rich <43017362+corey-rich@users.noreply.github.com>
2019-10-24 19:25:53 -0400

    New GUI

---
+++ Zork.Builder/ZorkMainForm.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Zork.Builder
+{
+    partial class ZorkMainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.DeleteButton = new System.Windows.Forms.Button();
+            this.OpenLabel = new System.Windows.Forms.Label();
+            this.FileTextBox = new System.Windows.Forms.TextBox();
+            this.OpenFileButton = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.RoomsLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 95);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(244, 404);
+            this.listBox1.TabIndex = 2;
+            // 
+            // AddButton
+            // 
+            this.AddButton.Location = new System.Drawing.Point(12, 505);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(100, 40);
+            this.AddButton.TabIndex = 3;
+            this.AddButton.Text = "&Add";
+            this.AddButton.UseVisualStyleBackColor = true;
+            // 
+            // DeleteButton
+            // 
+            this.DeleteButton.Location = new System.Drawing.Point(156, 505);
+            this.DeleteButton.Name = "DeleteButton";
+            this.DeleteButton.Size = new System.Drawing.Size(100, 40);
+            this.DeleteButton.TabIndex = 4;
+            this.DeleteButton.Text = "Delete";
+            this.DeleteButton.UseVisualStyleBackColor = true;
+            // 
+            // OpenLabel
+            // 
+            this.OpenLabel.AutoSize = true;
+            this.OpenLabel.Location = new System.Drawing.Point(12, 9);
+            this.OpenLabel.Name = "OpenLabel";
+            this.OpenLabel.Size = new System.Drawing.Size(69, 17);
+            this.OpenLabel.TabIndex = 5;
+            this.OpenLabel.Text = "Open File";
+            // 
+            // FileTextBox
+            // 
+            this.FileTextBox.Location = new System.Drawing.Point(88, 6);
+            this.FileTextBox.Name = "FileTextBox";
+            this.FileTextBox.Size = new System.Drawing.Size(858, 22);
+            this.FileTextBox.TabIndex = 6;
+            // 
+            // OpenFileButton
+            // 
+            this.OpenFileButton.Location = new System.Drawing.Point(953, 6);
+            this.OpenFileButton.Name = "OpenFileButton";
+            this.OpenFileButton.Size = new System.Drawing.Size(52, 23);
+            this.OpenFileButton.TabIndex = 7;
+            this.OpenFileButton.Text = "...";
+            this.OpenFileButton.UseVisualStyleBackColor = true;
+            this.OpenFileButton.Click += new System.EventHandler(this.OpenFileButton_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.Filter = "World Files (*json)|*.json";
+            // 
+            // RoomsLabel
+            // 
+            this.RoomsLabel.AutoSize = true;
+            this.RoomsLabel.Location = new System.Drawing.Point(111, 75);
+            this.RoomsLabel.Name = "RoomsLabel";
+            this.RoomsLabel.Size = new System.Drawing.Size(52, 17);
+            this.RoomsLabel.TabIndex = 8;
+            this.RoomsLabel.Text = "Rooms";
+            this.RoomsLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ZorkMainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.RoomsLabel);
+            this.Controls.Add(this.OpenFileButton);
+            this.Controls.Add(this.FileTextBox);
+            this.Controls.Add(this.OpenLabel);
+            this.Controls.Add(this.DeleteButton);
+            this.Controls.Add(this.AddButton);
+            this.Controls.Add(this.listBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "ZorkMainForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.Button DeleteButton;
+        private System.Windows.Forms.Label OpenLabel;
+        private System.Windows.Forms.TextBox FileTextBox;
+        private System.Windows.Forms.Button OpenFileButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Label RoomsLabel;
+    }
+}
+
---
+++ Zork.Builder/ZorkMainForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Forms;
+using Zork.Common;
+using Newtonsoft.Json;
+using System.IO;
+using Zork.Builder.ViewModels;
+
+namespace Zork.Builder
+{
+    public partial class ZorkMainForm : Form
+    {
+        private WorldViewModel ViewModel
+        { 
+            get => mViewModel; 
+            set => mViewModel = value; 
+        }
+
+        public ZorkMainForm()
+        {
+            InitializeComponent();
+
+            //Player player = new Player();
+            
+        }
+
+        private void OpenFileButton_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                ViewModel.World = JsonConvert.DeserializeObject<World>(File.ReadAllText(openFileDialog.FileName));
+                ViewModel.Filename = openFileDialog.FileName;
+            }
+        }
+
+        private WorldViewModel mViewModel;
+    }
+}
